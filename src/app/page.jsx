e49da11d7904b2e9f1d62180eb8340a272749ef8
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-11-19 18:29:32 +0500

    added some assets

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,10 @@
+import Hero from '@/components/Hero';
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <a className='abc' href="">Home</a>
-      <a className='abc' href="">Home</a>
-      <a className='abc' href="">Home</a>
-      <a className='abc' href="">Home</a>
-      <a className='abc' href="">Home</a>
+      <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import { TypeAnimation } from "react-type"
+
+const Hero = () => {
+    return (
+        <section>Hero</section>
+    )
+}
+
+export default Hero;
