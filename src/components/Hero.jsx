ed33e024ef1a57fc95023a2af26e4b1704a7d687
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-11-20 23:24:07 +0500

    start working on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <section>Hero</section>
+        <section className='h-[800px] relative bg-accent/10 xl:bg-white'>
+            <div>Header</div>
+            <div className="container mx-auto h-full">
+                <div className="relative z-20 h-full w-ful xl:max-w-[768px] flex flex-col items-center xl:items-start justify-center text-center">
+                    
+                </div>
+            </div>
+        </section>
     )
 }
 
