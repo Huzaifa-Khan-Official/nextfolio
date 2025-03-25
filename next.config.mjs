7cc088675296454b48a76bf0b7923a8a46ffefbd
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2025-03-25 15:26:17 +0500

    testing pwa configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,48 @@
+// import pwa from 'next-pwa'
+
+// const withPWA = pwa({
+//   dest: 'public',
+//   buildExcludes: [/app-build-manifest\.json$/, /middleware-manifest\.json$/],
+//   register: true,
+// })
+// const nextConfig = {
+//   output: 'standalone',
+//   sassOptions: {
+//     silenceDeprecations: ['legacy-js-api'],
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//       },
+//     ],
+//   },
+// };
+
+// export default withPWA(nextConfig)
+
 import pwa from 'next-pwa'
 
 const withPWA = pwa({
   dest: 'public',
-  buildExcludes: [/app-build-manifest\.json$/, /middleware-manifest\.json$/],
+  disable: process.env.NODE_ENV === 'development',
   register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
 })
+
 const nextConfig = {
   output: 'standalone',
   sassOptions: {
