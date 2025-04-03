1174e7f9e37f46e3ef644cd1b98e62bd2a701ba8
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2025-04-03 13:54:05 +0500

    implement email sending functionality with nodemailer; add API route for handling contact form submissions; update package.json with new dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
   sassOptions: {
     silenceDeprecations: ['legacy-js-api'],
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
